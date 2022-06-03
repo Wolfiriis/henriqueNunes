2908f55f59236108a06bea8b2d851e8e1640cd37
Wolfiriis <104776085+Wolfiriis@users.noreply.github.com>
2022-06-03 10:11:04 +0100

    Proj SB

---
+++ searchBar/searchBar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Linq;
+
+namespace searchBar
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n ###################### \n");
+            Console.WriteLine("        SearchBar   ");
+            Console.WriteLine("\n ###################### \n");
+
+
+            Console.WriteLine("\n ------------------ \n");
+            string[] palavras = { "week", "database", "army", "story", "assumption", "innocent", "messy", "dusty", "icy", "keen" };
+            List<string> lista = palavras.ToList();
+
+            Console.WriteLine(String.Join("\n", lista));
+            Console.WriteLine("\n ------------------ \n");
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Ordenar Lista");
+            Console.WriteLine("0 - Sair");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("\n ------------------ \n");
+                    lista.Sort();
+                    Console.WriteLine(String.Join("\n", lista));
+                    Console.WriteLine("\n ------------------ \n");
+                    break;
+
+                case "0":
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+
+    }
+}
